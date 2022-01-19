5bfc66b5b3f204d1493f93195236021cdc2e10e9
maksymilianMroz <mrozmaksymilian@gmail.com>
2022-01-19 03:14:19 +0100

    Updates

---
+++ precache-manifest.c11c00f63c4f8054670e04cb65c839a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d774680df150752ed909c773c742667e",
+    "url": "/product-list-management/index.html"
+  },
+  {
+    "revision": "8b590de6ea8078d2bb98",
+    "url": "/product-list-management/static/css/main.77951209.chunk.css"
+  },
+  {
+    "revision": "89b90cd99e53e4b14571",
+    "url": "/product-list-management/static/js/2.eccf45a3.chunk.js"
+  },
+  {
+    "revision": "8b590de6ea8078d2bb98",
+    "url": "/product-list-management/static/js/main.c1218261.chunk.js"
+  },
+  {
+    "revision": "d112483fb8333fd0603e",
+    "url": "/product-list-management/static/js/runtime~main.3c2368d5.js"
+  }
+]);
